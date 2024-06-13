639e25a4729844ffc5d3e2960c1c93a119db4807
Parviz Shirinov <parveeezy@gmail.com>
2024-06-13 22:13:12 +0300

    hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
 import {loadingAC} from './bll/loadingReducer'
@@ -14,13 +14,14 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const dispatch = useDispatch();
+    const isLoading = useSelector<AppStoreType>(state => state.loading.isLoading)
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
-        // setTimeout
+    const setLoading = () => {
+        dispatch(loadingAC(true))
+        setTimeout(() => {
+            dispatch(loadingAC(false));
+        }, 1500);
     }
 
     return (
@@ -28,7 +29,7 @@ const HW10 = () => {
             <div className={s2.hwTitle}>Homework #10</div>
 
             <div className={s2.hw}>
-                {isLoading ? (
+                {isLoading === true ? (
                     <div id={'hw10-loading'}>
                         <Loader/>
                     </div>
